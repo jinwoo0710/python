eaa6f40b04d3e1e37ebfcb028c91f189efd1c0ef
jinwoo0710 <80086169+jinwoo0710@users.noreply.github.com>
2021-04-09 15:48:19 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,11 @@
+fstName = ['k', 'i', 'm']
+sndName = ['jin', 'woo']
+stdNum = [2, 0, 2, 1, 0, 6, 0, 4, 6]
+
+print(fstName)
+print(sndName)
+# 화면에 k, j, w를 출력하는 프로그램을 작성하시오.
+print(fstName[0]) # k
+                  # j
+                  # w
+
